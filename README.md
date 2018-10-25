8c3511a15cf9165e23e63ef203cb3de3652538b0
LEDUNOIS Simon <simon.ledunois@cgi.com>
2018-10-25 11:36:46 +0200

    Add Licence and add default conf in docker compose

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Compétences   
+# À propos de l'application Compétences  
+* Licence : [AGPL v3](http://www.gnu.org/licenses/agpl.txt) - Copyright Département de la Seine-et-Marne, CGI, Conseil Régional des Hauts-de-France
+* Développeur(s) : CGI
+* Financeur(s) : Département de la Seine-et-Marne, CGI, Conseil Régional des Hauts-de-France
+
+* Description : Application de gestion des compétences
--- src/main/resources/public/ts/models/common/DefaultEnseignement.ts
+++ src/main/resources/public/ts/models/common/DefaultEnseignement.ts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) Région Hauts-de-France, Département de la Seine-et-Marne, CGI, 2016.
+ *     This file is part of OPEN ENT NG. OPEN ENT NG is a versatile ENT Project based on the JVM and ENT Core Project.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation (version 3 of the License).
+ *   For the sake of explanation, any module that communicate over native
+ *   Web protocols, such as HTTP, with OPEN ENT NG is outside the scope of this
+ *   license and could be license under its own terms. This is merely considered
+ *   normal use of OPEN ENT NG, and does not fall under the heading of "covered work".
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 import { Model, Collection } from 'entcore';
 import { Competence } from "../parent_eleve/Competence";
 
