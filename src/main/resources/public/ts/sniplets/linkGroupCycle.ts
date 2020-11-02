cec1cff4aa26cbe8e9750debf3155011f6b76f07
Agnes Lapeyronnie <agnes.lapeyronnie@cgi.com>
2020-11-02 12:36:01 +0100

    [MN-1288][CO-1139] Pouvoir retirer un groupe ou un classe d'un cycle.

--- src/main/resources/public/ts/sniplets/linkGroupCycle.ts
+++ src/main/resources/public/ts/sniplets/linkGroupCycle.ts
@@ -15,7 +15,7 @@
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-import {_, http, notify} from 'entcore';
+import {_, idiom as lang, http, notify} from 'entcore';
 import {Classe} from "../models/teacher";
 import * as utils from "../utils/teacher";
 
@@ -29,7 +29,7 @@ export const linkGroupCycle = {
             this.groupLibelle = [];
             this.search = {
                 name : "",
-                type :{ all : true},
+                type : {all : true},
                 id_cycle : undefined,
                 hasChange : false,
                 classeAll : false
@@ -42,24 +42,29 @@ export const linkGroupCycle = {
             this.cycles = this.source.cycles;
             this.$parent.filterLinkSearch = this.filterLinkSearch;
             this.selectedClasses = [];
-            this.opened.lightboxLinkGroupCycle = false;
+            this.opened = {
+                lightboxLinkGroupCycle : false,
+                lightboxDataOnDevoirs: false,
+            };
             this.selectedRadio = {id_cycle: undefined};
             this.dataOnClasses = [];
             linkGroupCycle.that = this;
             this.getClasses();
         },
+
         initSource: function () {
         },
+
         getClasses: function () {
-            http().get(`/viescolaire/classes?idEtablissement=${this.idStructure}`)
-                .done(function (res) {
-                    this.classesGroupes = {
-                        all: this.castClasses(res)
-                    };
-                    console.log('load sniplet link group cycle');
-                    this.$apply('classesGroupes');
-                }.bind(this));
+            http().get(`/viescolaire/classes?idEtablissement=${this.idStructure}`).done(function (res) {
+                this.classesGroupes = {
+                    all: this.castClasses(res)
+                };
+                console.log('load sniplet link group cycle');
+                this.$apply('classesGroupes');
+            }.bind(this));
         },
+
         checkDataOnClasses: function(classes){
             let jsonToSave = {
                 idClasses: _.pluck(_.filter(classes, function (classe) {
@@ -68,36 +73,33 @@ export const linkGroupCycle = {
                 id_cycle: parseInt(linkGroupCycle.that.selectedRadio.id_cycle),
             };
             if (jsonToSave.idClasses.length > 0 && !isNaN(jsonToSave.id_cycle)) {
-                http().postJson(`competences/link/check/data/classes`, jsonToSave)
-                    .done((res) => {
-                        if (res.length > 0) {
-                            _.forEach(res, function (devoir) {
-                                devoir.classe = _.findWhere(linkGroupCycle.that.classesGroupes.all,
-                                    {id: devoir.id_groupe}).name;
-                            });
-                            linkGroupCycle.that.dataOnClasses = res;
-                            linkGroupCycle.that.opened.lightboxDataOnDevoirs = true;
-                        }
-                        else {
-                            linkGroupCycle.that.dataOnClasses = [];
-                            linkGroupCycle.that.opened.lightboxDataOnDevoirs = false;
-                            linkGroupCycle.that.save(classes);
-                        }
-                        linkGroupCycle.that.opened.lightboxLinkGroupCycle = false;
+                http().postJson(`competences/link/check/data/classes`, jsonToSave).done((res) => {
+                    if (res.length > 0) {
+                        _.forEach(res, function (devoir) {
+                            devoir.classe = _.findWhere(linkGroupCycle.that.classesGroupes.all,
+                                {id: devoir.id_groupe}).name;
+                        });
+                        linkGroupCycle.that.dataOnClasses = res;
+                        linkGroupCycle.that.opened.lightboxDataOnDevoirs = true;
+                    }
+                    else {
+                        linkGroupCycle.that.dataOnClasses = [];
+                        linkGroupCycle.that.opened.lightboxDataOnDevoirs = false;
+                        linkGroupCycle.that.save(classes);
+                    }
+                    linkGroupCycle.that.opened.lightboxLinkGroupCycle = false;
+                    utils.safeApply(this);
+                }).error(function (res) {
+                    if (res.status === 401) {
+                        notify.error('evaluation.error.unautorize');
                         utils.safeApply(this);
-                    })
-                    .error(function (res) {
-                        if (res.status === 401) {
-                            notify.error('evaluation.error.unautorize');
-                            utils.safeApply(this);
-                        } else {
-                            notify.error('evaluation.link.error.request');
-                        }
-                        utils.safeApply(this);
-                    }).bind(this);
+                    } else {
+                        notify.error('evaluation.link.error.request');
+                    }
+                    utils.safeApply(this);
+                }).bind(this);
             }
             else {
-
                 if (isNaN(jsonToSave.id_cycle)) {
                     notify.error("evaluation.link.no.cycle");
                 }
@@ -113,6 +115,7 @@ export const linkGroupCycle = {
                 utils.safeApply(this);
             }
         },
+
         save: function (classes) {
             let classesToUpdate = _.filter(classes, function (classe) {
                 return classe.id_cycle !== parseInt(linkGroupCycle.that.selectedRadio.id_cycle);
@@ -122,54 +125,62 @@ export const linkGroupCycle = {
                 id_cycle: parseInt(linkGroupCycle.that.selectedRadio.id_cycle),
                 typesGroupes: _.pluck(classesToUpdate, 'type_groupe')
             };
-            http().putJson(`competences/link/groupes/cycles`, jsonToSave)
-                .done(() => {
+            http().putJson(`competences/link/groupes/cycles`, jsonToSave).done(() => {
+                if(linkGroupCycle.that.selectedRadio.id_cycle != 0) {
                     notify.info('evaluation.link.success');
-                    _.forEach(classes, (classe) => {
-                        classe.selected = false;
-                        classe.id_cycle = jsonToSave.id_cycle;
-                    });
-                    linkGroupCycle.that.opened.lightboxLinkGroupCycle = false;
-                    linkGroupCycle.that.opened.lightboxDataOnDevoirs = false;
+                }
+                else {
+                    notify.info('evaluation.unlink.success');
+                }
+                _.forEach(classes, (classe) => {
+                    classe.selected = false;
+                    classe.id_cycle = jsonToSave.id_cycle;
+                });
+                linkGroupCycle.that.opened.lightboxLinkGroupCycle = false;
+                linkGroupCycle.that.opened.lightboxDataOnDevoirs = false;
+                utils.safeApply(this);
+            }).error(function (res) {
+                if (res.status === 401) {
+                    notify.error('evaluation.error.unautorize');
                     utils.safeApply(this);
-                })
-                .error(function (res) {
-                    if (res.status === 401) {
-                        notify.error('evaluation.error.unautorize');
-                        utils.safeApply(this);
-                    }else {
-                        notify.error('evaluation.link.error.request');
-                    }
-                    utils.safeApply(this);
-                }).bind(this);
+                } else {
+                    notify.error('evaluation.link.error.request');
+                }
+                utils.safeApply(this);
+            }).bind(this);
         },
-       castClasses: (classes) => {
+
+        castClasses: (classes) => {
             return _.map(classes, (classe) => {
                 classe.type_groupe_libelle = Classe.get_type_groupe_libelle(classe);
                 classe = new Classe(classe);
                 return classe;
             });
         },
+
         getClasseCycle: (id_cycle) => {
             let _cycle = _.findWhere(linkGroupCycle.that.cycles, {id_cycle: parseInt(id_cycle)});
-            return (_cycle !== undefined) ?  _cycle.libelle : '';
+            return (_cycle !== undefined) ? _cycle.libelle : lang.translate("viescolaire.utils.no.cycle");
         },
-        selectedClasse: (classe) => {
-            classe.selected = ! classe.selected;
 
+        selectedClasse: (classe) => {
+            classe.selected = !classe.selected;
         },
+
         selectType: (type,search) => {
             type.isSelected = !type.isSelected;
             search.type[type.type] = !search.type[type.type];
         },
+
         selectAllType: () => {
             _.forEach(_.keys(linkGroupCycle.that.search.type), function (key) {
-                linkGroupCycle.that.search.type[key]= !linkGroupCycle.that.search.type.all;
+                linkGroupCycle.that.search.type[key] = !linkGroupCycle.that.search.type.all;
             });
             _.forEach(linkGroupCycle.that.groupLibelle, function (type) {
                 type.isSelected = linkGroupCycle.that.search.type.all;
             });
         },
+
         selectAllClasses: (classes) => {
             linkGroupCycle.that.search.classeAll = !linkGroupCycle.that.search.classeAll;
             _.forEach(classes, (classe) => {
@@ -177,6 +188,7 @@ export const linkGroupCycle = {
             });
             utils.safeApply(linkGroupCycle.that);
         },
+
         filterLinkSearch: function(searchParams){
             return (classe) => {
                 let result = true;
@@ -190,11 +202,12 @@ export const linkGroupCycle = {
                 if (result && searchParams.name && searchParams.name !== '*' && classe !== undefined
                     && searchParams.name !== '') {
                     let regexp = new RegExp('^' + searchParams.name.toUpperCase());
-                    result =  regexp.test(classe.name.toUpperCase());
+                    result = regexp.test(classe.name.toUpperCase());
                 }
                 return result;
             };
         },
+
         checkSelectedClasses: function (classes) {
             linkGroupCycle.that.selectedClasses = _.filter(classes, function (classe){
                 return classe.selected === true;
@@ -205,6 +218,7 @@ export const linkGroupCycle = {
 
             return linkGroupCycle.that.selectedClasses;
         },
+
         initRadio: function (classes) {
             linkGroupCycle.that.selectedClasses = _.filter(classes, function (classe){
                 return classe.selected === true;
@@ -215,6 +229,6 @@ export const linkGroupCycle = {
                     id_cycle: linkGroupCycle.that.selectedClasses[0].id_cycle.toString()
                 };
             }
-        }
+        },
     }
 };
