e2099e0e3bff5d8e14c4b6e57a2add08cc8fd82b
agnes.lapeyronnie <agnes.lapeyronnie@cgi.com>
2021-04-27 16:32:19 +0200

    fix(Rights) : EVAL-88 Modify positioning

--- src/main/java/fr/openent/competences/security/SaveAppreciationBilanPeriodiqueFilter.java
+++ src/main/java/fr/openent/competences/security/SaveAppreciationBilanPeriodiqueFilter.java
@@ -3,6 +3,7 @@ package fr.openent.competences.security;
 import fr.openent.competences.security.utils.WorkflowActionUtils;
 import fr.openent.competences.security.utils.WorkflowActions;
 import fr.wseduc.webutils.http.Binding;
+import fr.wseduc.webutils.request.RequestUtils;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpServerRequest;
@@ -13,7 +14,21 @@ import org.entcore.common.user.UserInfos;
 public class SaveAppreciationBilanPeriodiqueFilter implements ResourcesProvider {
     @Override
     public void authorize(HttpServerRequest resourceRequest, Binding binding, UserInfos user, Handler<Boolean> handler) {
-        MultiMap params = resourceRequest.params();
+        RequestUtils.bodyToJson(resourceRequest, resource -> {
+           if(!resource.containsKey("idClasse")){
+               resourceRequest.resume();
+               handler.handle(false);
+           } else {
+               JsonArray idClasses = new JsonArray().add(resource.getString("idClasse"));
+               WorkflowActionUtils.hasHeadTeacherRight(user, idClasses,null,null, null,null,
+                       null, event -> {
+                           Boolean isHeadTeacher = event.isRight() ? event.right().getValue() : false;
+                           handler.handle(isHeadTeacher ||
+                                   WorkflowActionUtils.hasRight(user, WorkflowActions.SAVE_APPRECIATION_BILAN_PERIODIQUE.toString()));
+                       });
+           }
+        });
+      /*  MultiMap params = resourceRequest.params();
         if(!params.contains("idClasse")){
             resourceRequest.resume();
             handler.handle(false);
@@ -25,6 +40,6 @@ public class SaveAppreciationBilanPeriodiqueFilter implements ResourcesProvider 
                         handler.handle(isHeadTeacher ||
                                 WorkflowActionUtils.hasRight(user, WorkflowActions.SAVE_APPRECIATION_BILAN_PERIODIQUE.toString()));
                     });
-        }
+        }*/
     }
 }
