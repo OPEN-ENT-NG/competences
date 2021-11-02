e996dba06d39b9f91cc6964fbe406a60b9fc5d56
Theo Beguey <theo.beguey@cgi.com>
2021-11-02 17:15:41 +0100

    Fix error no content.

--- src/main/java/fr/openent/competences/security/SetAvisConseilFilter.java
+++ src/main/java/fr/openent/competences/security/SetAvisConseilFilter.java
@@ -1,6 +1,7 @@
 package fr.openent.competences.security;
 
 import fr.openent.competences.AccessEventBus;
+import fr.openent.competences.Competences;
 import fr.openent.competences.security.utils.WorkflowActionUtils;
 import fr.openent.competences.security.utils.WorkflowActions;
 import fr.wseduc.webutils.http.Binding;
@@ -15,21 +16,30 @@ import org.entcore.common.user.UserInfos;
 public class SetAvisConseilFilter implements ResourcesProvider {
     @Override
     public void authorize(HttpServerRequest resourceRequest, Binding binding, UserInfos user, Handler<Boolean> handler) {
-        RequestUtils.bodyToJson(resourceRequest, params -> {
-            if(!params.containsKey("id_eleve") && !params.containsKey("id_structure")){
-                resourceRequest.resume();
-                handler.handle(false);
-            } else {
+        boolean hasRight = WorkflowActionUtils.hasRight(user, WorkflowActions.SET_AVIS_CONSEIL.toString());
+        EventBus eventBus = AccessEventBus.getInstance().getEventBus();
+
+        if ("POST".equals(resourceRequest.method().toString())) {
+            RequestUtils.bodyToJson(resourceRequest, params -> {
                 JsonArray idEleves = new JsonArray().add(params.getString("id_eleve"));
                 String idStructure = params.getString("id_structure");
-                EventBus eventBus = AccessEventBus.getInstance().getEventBus();
                 WorkflowActionUtils.hasHeadTeacherRight(user, null,null,null, idEleves,
                         eventBus, idStructure, event -> {
-                            Boolean isHeadTeacher = event.isRight() ? event.right().getValue() : false;
-                            handler.handle(isHeadTeacher ||
-                                    WorkflowActionUtils.hasRight(user, WorkflowActions.SET_AVIS_CONSEIL.toString()));
+                            boolean isHeadTeacher = event.isRight() ? event.right().getValue() : false;
+                            handler.handle(isHeadTeacher || hasRight);
                         });
-            }
-        });
+            });
+        } else if ("DELETE".equals(resourceRequest.method().toString())) {
+            JsonArray idEleves = new JsonArray().add(resourceRequest.params().get("id_eleve"));
+            String idStructure = resourceRequest.params().get("id_structure");
+            WorkflowActionUtils.hasHeadTeacherRight(user, null,null,null, idEleves,
+                    eventBus, idStructure, event -> {
+                        boolean isHeadTeacher = event.isRight() ? event.right().getValue() : false;
+                        handler.handle(isHeadTeacher || hasRight);
+                    });
+        } else {
+            resourceRequest.resume();
+            handler.handle(false);
+        }
     }
 }
