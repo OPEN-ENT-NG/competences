e8ff3a9652dad0bec2ac8837b07db1c46d77cf66
jinhda douang <s>
2021-12-08 23:00:53 +0100

    chore: add git action workflow ci and unit test

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "transform": {
+        ".(ts|tsx)": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+    },
+    "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js"
+    ],
+    "testPathIgnorePatterns": [
+        "/node_modules/",
+        "<rootDir>/competences/build/",
+        "<rootDir>/competences/out/"
+    ],
+    "verbose": true,
+    "testURL": "http://localhost/",
+    "coverageDirectory": "coverage/front",
+    "coverageReporters": [
+        "text",
+        "cobertura"
+    ]
+};
