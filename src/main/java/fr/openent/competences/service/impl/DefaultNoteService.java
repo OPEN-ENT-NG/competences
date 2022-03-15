d77c1a93fb1f310485a071ded0c6ee4fa0870903
florent.mariotti <florent.mariotti@cgi.com>
2022-03-15 17:13:34 +0100

    fix(RelevePeriodique): EVAL-293 Fixed releve sometimes not displaying because of null finale average.

--- src/main/java/fr/openent/competences/service/impl/DefaultNoteService.java
+++ src/main/java/fr/openent/competences/service/impl/DefaultNoteService.java
@@ -1348,13 +1348,15 @@ public class DefaultNoteService extends SqlCrudService implements NoteService {
             moyFinalesEleves.forEach(moyFinale -> {
                 JsonObject moyFinaleJson = (JsonObject) moyFinale;
                 Long idP = moyFinaleJson.getLong("id_periode");
-                NoteDevoir noteEleve = new NoteDevoir(Double.valueOf(moyFinaleJson.getString("moyenne")),
-                        20.0, false, 1.0, moyFinaleJson.getString("id_eleve"));
-                if(!notesByDevoirByPeriodeClasse.containsKey(idP))
-                    notesByDevoirByPeriodeClasse.put(idP, new HashMap<>());
-                if(!notesByDevoirByPeriodeClasse.get(idP).containsKey(idP))
-                    notesByDevoirByPeriodeClasse.get(idP).put(idP, new ArrayList<>());
-                notesByDevoirByPeriodeClasse.get(idP).get(idP).add(noteEleve);
+                if(moyFinaleJson.getString("moyenne") != null){
+                    NoteDevoir noteEleve = new NoteDevoir(Double.valueOf(moyFinaleJson.getString("moyenne")),
+                            20.0, false, 1.0, moyFinaleJson.getString("id_eleve"));
+                    if(!notesByDevoirByPeriodeClasse.containsKey(idP))
+                        notesByDevoirByPeriodeClasse.put(idP, new HashMap<>());
+                    if(!notesByDevoirByPeriodeClasse.get(idP).containsKey(idP))
+                        notesByDevoirByPeriodeClasse.get(idP).put(idP, new ArrayList<>());
+                    notesByDevoirByPeriodeClasse.get(idP).get(idP).add(noteEleve);
+                }
             });
         }
 
