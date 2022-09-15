bd226f65fe0dc564511017a6a842bee15f9b22b5
Seuret Rémy <remy.seuret@cgi.com>
2022-09-15 13:43:34 +0200

    fix(Transition):EVAL-373 alter table for SAAS (#107)

---
+++ src/main/resources/sql/036-match_class_id_transition.sql
@@ -0,0 +1,3 @@
+ALTER TABLE notes.match_class_id_transition ALTER COLUMN old_class_id TYPE varchar(255);
+ALTER TABLE notes.match_class_id_transition ALTER COLUMN external_id TYPE varchar(255);
+ALTER TABLE notes.match_class_id_transition ALTER COLUMN new_class_id TYPE varchar(255);
