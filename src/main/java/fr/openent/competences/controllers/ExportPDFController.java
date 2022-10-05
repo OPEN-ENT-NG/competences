cf0c8525e52465f68973b59fe4bc1a0357ce310f
Florent Mariotti <florentcorse@gmail.com>
2022-10-05 14:13:28 +0200

    fix(ExportPDF) : EVAL-391 Fixed student marks PDF having a 2 decimals average (#131)
    * fix(ExportPDF) : EVAL-391 Fixed student marks PDF having a 2 decimals average instead of 1 decimal average like everything else.
    
    * fix(ExportPDF) : EVAL-391 Text fix.
    
    Co-authored-by: florent.mariotti <florent.mariotti@cgi.com>

--- src/main/java/fr/openent/competences/controllers/ExportPDFController.java
+++ src/main/java/fr/openent/competences/controllers/ExportPDFController.java
@@ -1108,8 +1108,8 @@ public class ExportPDFController extends ControllerHelper {
                                         note.put("persoColor", niveau.getString("persoColor"));
                                         note.put("nonEvalue", false);
 
-                                        DecimalFormat decimalFormat = new DecimalFormat("#0.0");
-                                        decimalFormat.setRoundingMode(RoundingMode.HALF_UP);//with this mode 2.125 -> 2.13 without 2.125 -> 2.12
+                                        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+                                        decimalFormat.setRoundingMode(RoundingMode.HALF_UP);
 
                                         String moyCalcule = decimalFormat.format(resultNote.getDouble(MOYENNE).doubleValue());
                                         if (isHabilite)
