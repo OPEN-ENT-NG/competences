ef549e5e2e40adaad715ba4d1d18ccf09b8cf6c7
simon.chaval <simon.chaval@cgi.com>
2022-10-14 18:43:20 +0200

    fix(ÃreportModel): #EVAL-174 fix rebase error and return from pr

---
+++ src/main/java/fr/openent/competences/security/modelbulletinrights/PostModelExportBulletion.java
@@ -0,0 +1,27 @@
+package fr.openent.competences.security.modelbulletinrights;
+
+import fr.openent.competences.constants.Field;
+import fr.openent.competences.security.utils.FilterUserUtils;
+import fr.openent.competences.security.utils.WorkflowActions;
+import fr.wseduc.webutils.http.Binding;
+import fr.wseduc.webutils.request.RequestUtils;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServerRequest;
+import org.entcore.common.http.filter.ResourcesProvider;
+import org.entcore.common.user.UserInfos;
+
+import static fr.openent.competences.security.utils.WorkflowActionUtils.hasRight;
+
+public class PostModelExportBulletion implements ResourcesProvider {
+    @Override
+    public void authorize(HttpServerRequest request, Binding binding, UserInfos user, Handler<Boolean> handler) {
+        // Verify if idStructure front equals idStructure user && user right export.bulletins.periodique
+        RequestUtils.bodyToJson(request, body -> {
+            String structureId = body.getString(Field.STRUCTUREID);
+            if (structureId == null) {
+                handler.handle(false);
+            }
+            handler.handle(new FilterUserUtils(user,null).validateStructure(structureId) && hasRight(user, WorkflowActions.ACCESS_MODEL_BULLETIN.toString()));
+        });
+    }
+}
