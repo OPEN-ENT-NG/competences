6bf1fbc75f13b06cda0569e25a5b5c1261b6ee5e
BOES <gregoryboes33@gmail.com>
2023-01-05 17:09:20 +0100

    fix(security): EVAL-438 rebase dev

---
+++ src/main/java/fr/openent/competences/security/CreateAvisConseilBilanPeriodique.java
@@ -0,0 +1,16 @@
+package fr.openent.competences.security;
+
+import fr.openent.competences.constants.Field;
+import fr.openent.competences.security.utils.WorkflowActionUtils;
+import fr.openent.competences.security.utils.WorkflowActions;
+import fr.wseduc.webutils.http.Binding;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServerRequest;
+import org.entcore.common.http.filter.ResourcesProvider;
+import org.entcore.common.user.UserInfos;
+
+public class CreateAvisConseilBilanPeriodique implements ResourcesProvider {
+    @Override
+    public void authorize(HttpServerRequest request, Binding binding, UserInfos user, Handler<Boolean> handler) {
+        handler.handle(WorkflowActionUtils.hasRight(user, WorkflowActions.CREATE_AVIS_CONSEIL_BILAN_PERIODIQUE.toString()));}
+}
