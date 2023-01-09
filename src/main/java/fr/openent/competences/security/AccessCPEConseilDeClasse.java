20132e84b43be663127a14c34e4ccc4b9241cf0e
BOES <gregoryboes33@gmail.com>
2023-01-09 14:11:18 +0100

    fix(security): wip routes test

--- src/main/java/fr/openent/competences/security/AccessCPEConseilDeClasse.java
+++ src/main/java/fr/openent/competences/security/AccessCPEConseilDeClasse.java
@@ -1,5 +1,6 @@
 package fr.openent.competences.security;
 
+import fr.openent.competences.constants.Field;
 import fr.openent.competences.security.utils.WorkflowActionUtils;
 import fr.openent.competences.security.utils.WorkflowActions;
 import fr.wseduc.webutils.http.Binding;
@@ -8,8 +9,7 @@ import io.vertx.core.http.HttpServerRequest;
 import org.entcore.common.http.filter.ResourcesProvider;
 import org.entcore.common.user.UserInfos;
 
-public class AccessConseilDeClasse implements ResourcesProvider {
-
+public class AccessCPEConseilDeClasse implements ResourcesProvider {
     @Override
     public void authorize(HttpServerRequest request, Binding binding, UserInfos user, Handler<Boolean> handler) {
         handler.handle(WorkflowActionUtils.hasRight(user, WorkflowActions.ACCESS_CONSEIL_DE_CLASSE.toString()));
