b439db22443bd3a51ae12e85644ee1d3efc6c25b
BOES <gregoryboes33@gmail.com>
2023-01-09 16:20:22 +0100

    fix(security): add new security classes to check structureId

--- src/main/java/fr/openent/competences/security/AccessChildrenParentFilterStructureId.java
+++ src/main/java/fr/openent/competences/security/AccessChildrenParentFilterStructureId.java
@@ -12,22 +12,25 @@ import org.entcore.common.user.UserInfos;
 
 import static fr.openent.competences.Competences.ID_ELEVE_KEY;
 
-public class AccessChildrenParentFilter implements ResourcesProvider {
+public class AccessChildrenParentFilterStructureId implements ResourcesProvider {
     @Override
     public void authorize(final HttpServerRequest request, Binding binding, UserInfos user, Handler<Boolean> handler) {
+        final String idStructure = request.params().get(Field.IDETABLISSEMENT);
+        final boolean isInStructure = user.getStructures().contains(idStructure);
+
         boolean isAdminTeacherPersonnel = WorkflowActionUtils.hasRight(user, WorkflowActions.ADMIN_RIGHT.toString())
                 || "Personnel".equals(user.getType()) || "Teacher".equals(user.getType());
 
         if ("GET".equals(request.method().toString())) {
             handler.handle(isAdminTeacherPersonnel
-                    || user.getUserId().equals(request.params().get(ID_ELEVE_KEY))
-                    || user.getChildrenIds().contains(request.params().get(ID_ELEVE_KEY))
+                    || isInStructure && user.getUserId().equals(request.params().get(ID_ELEVE_KEY))
+                    || isInStructure && user.getChildrenIds().contains(request.params().get(ID_ELEVE_KEY))
             );
         } else {
             RequestUtils.bodyToJson(request, params -> {
                 handler.handle(isAdminTeacherPersonnel
-                        || user.getUserId().equals(params.getString(ID_ELEVE_KEY))
-                        || user.getChildrenIds().contains(params.getString(ID_ELEVE_KEY))
+                        || isInStructure && user.getUserId().equals(params.getString(ID_ELEVE_KEY))
+                        || isInStructure && user.getChildrenIds().contains(params.getString(ID_ELEVE_KEY))
                 );
             });
         }
