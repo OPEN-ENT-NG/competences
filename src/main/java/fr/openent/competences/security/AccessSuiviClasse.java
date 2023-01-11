54e4033fd3fe2c5545f9d2791dc567aa3e72e76a
BOES <gregoryboes33@gmail.com>
2023-01-11 15:08:21 +0100

    fix(security): #EVAL-456 create ACCESS SUIVI CLASSE

--- src/main/java/fr/openent/competences/security/AccessSuiviClasse.java
+++ src/main/java/fr/openent/competences/security/AccessSuiviClasse.java
@@ -1,21 +1,14 @@
 package fr.openent.competences.security;
 
-import fr.openent.competences.Competences;
-import fr.openent.competences.constants.Field;
 import fr.openent.competences.security.utils.FilterUserUtils;
 import fr.openent.competences.security.utils.WorkflowActionUtils;
 import fr.openent.competences.security.utils.WorkflowActions;
 import fr.wseduc.webutils.http.Binding;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.json.JsonArray;
 import org.entcore.common.http.filter.ResourcesProvider;
-import org.entcore.common.sql.Sql;
-import org.entcore.common.sql.SqlResult;
 import org.entcore.common.user.UserInfos;
 
-import static fr.openent.competences.Competences.ID_CLASSE_KEY;
-import static fr.openent.competences.Competences.ID_STRUCTURE_KEY;
 
 public class AccessSuiviClasse implements ResourcesProvider {
     @Override
