5653776afd09d497120dc87e34a6bb0e8b350c46
simon.chaval <simon.chaval@cgi.com>
2023-01-20 10:37:02 +0100

    fix(security): #EVAL-456 Unit tests for security

--- src/test/java/fr/openent/competences/security/AccessEvaluationFilterTest.java
+++ src/test/java/fr/openent/competences/security/AccessEvaluationFilterTest.java
@@ -48,105 +48,40 @@ public class AccessEvaluationFilterTest {
     HttpServerRequest request;
     Binding binding;
     MultiMap map;
-    List<String> structures;
     UserInfos user = new UserInfos();
     List<UserInfos.Action> actions;
-    Devoir devoir;
-    List<String> childrenIds;
     UserInfos.Action role1;
 
     FilterDevoirUtils filterDevoirUtils;
     @Before
     public void setUp() throws NoSuchFieldException {
+        access = new AccessEvaluationFilter();
+        filterDevoirUtils = new FilterDevoirUtils();
         request = Mockito.mock(HttpServerRequest.class);
         binding = Mockito.mock(Binding.class);
-        map = Mockito.spy(new HeadersAdaptor(new DefaultHttpHeaders()));
-        structures = new ArrayList<>();
-        user = mock(UserInfos.class);
+        user = new UserInfos();
         actions = new ArrayList<>();
         role1 = new UserInfos.Action();
-        childrenIds = new ArrayList<>();
-        devoir = Mockito.mock(Devoir.class);
-        filterDevoirUtils = Mockito.spy(new FilterDevoirUtils());
-        access = new AccessEvaluationFilter();
+        map = Mockito.spy(new HeadersAdaptor(new DefaultHttpHeaders()));
     }
-
-
-
     @Test
     public void testAuthorizeAdmin(TestContext ctx){
-        //a tester si user est admin => return true;
-        // s'il n'est pas admin, tester s'il est enseignant
-            //s'il est enseignant:
-                //Tester si la requete contient un parametre IDDEVOIR
-                    //Si elle ne contient pas le paramètre IDDEVOIR => on renvoie false
-                    //Si on a bien le param IDDEVOIR:
-                        //On récupère l'id du devoir et on regarde si le user peut y accéder
-                        //sinon on renvoie false
-            //Si pas enseignant : on renvoie false
         role1.setDisplayName(WorkflowActions.ADMIN_RIGHT.toString());
         actions.add(role1);
-        Async async = ctx.async();
         user.setAuthorizedActions(actions);
         access.authorize(request, binding, user, result -> {
             ctx.assertEquals(true, result);
-            async.complete();
         });
-        async.awaitSuccess(10000);
+    }
+    @Test
+    public void testAuthorizeNoAdmin(TestContext ctx){
+        role1.setDisplayName(WorkflowActions.COMPETENCES_ACCESS.toString());
+        actions.add(role1);
+        user.setAuthorizedActions(actions);
+        access.authorize(request, binding, user, result -> {
+            ctx.assertEquals(false, result);
+        });
     }
 
-//    @Test
-//    public void testAuthorizeTeacherWithGoodDevoirID(TestContext ctx) throws Exception {
-//        filterDevoirUtils = mock(FilterDevoirUtils.class);
-//        access = Mockito.spy(new AccessEvaluationFilter());
-//        PowerMockito.whenNew(AccessEvaluationFilter.class).withNoArguments().thenReturn(access);
-//        PowerMockito.whenNew(FilterDevoirUtils.class).withNoArguments().thenReturn(filterDevoirUtils);
-//        //PowerMockito.whenNew(AccessEvaluationFilter.class).withArguments(filterDevoirUtils).thenReturn(access);
-//        doAnswer(invocation -> {
-//            Handler<Boolean> handler = invocation.getArgument(2);
-//            handler.handle(true);
-//            return null;
-//        }).when(filterDevoirUtils).validateAccessDevoir(anyLong(), any(), any());
-//
-//        when(user.getType()).thenReturn("Teacher");
-//        map.set(Field.IDDEVOIR, "1111111");
-//        Mockito.doReturn(map).when(request).params();
-//
-//        role1.setDisplayName(WorkflowActions.ADMIN_RIGHT.toString());
-//        actions.add(role1);
-//        user.setAuthorizedActions(actions);
-//        Async async = ctx.async();
-//        access.authorize(request, null, user, result -> {
-//            ctx.assertEquals(true, result);
-//            async.complete();
-//        });
-//        async.awaitSuccess(10000);
-//    }
-//
-//
-//    @Test
-//    public void testAuthorizeTeacherWithBadDevoirID(TestContext ctx) {
-//        filterDevoirUtils = mock(FilterDevoirUtils.class);
-//        access = mock(AccessEvaluationFilter.class);
-//        try {
-//            PowerMockito.whenNew(AccessEvaluationFilter.class).withArguments(filterDevoirUtils).thenReturn(access);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        role1.setDisplayName(WorkflowActions.ADMIN_RIGHT.toString());
-//        actions.add(role1);
-//        user.setAuthorizedActions(actions);
-//
-//        when(user.getType()).thenReturn("Teacher");
-//        map.set(Field.IDDEVOIR, "1111111");
-//        Mockito.doReturn(map).when(request).params();
-//
-//        Async async = ctx.async();
-//        user.setAuthorizedActions(actions);
-//        access.authorize(request, binding, user, result -> {
-//            ctx.assertEquals(true, result);
-//            async.complete();
-//        });
-//        async.awaitSuccess(10000);
-//    }
+
 }
