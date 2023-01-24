1eed071600ee1fc7a12392bf92b433be84130559
BOES <gregoryboes33@gmail.com>
2023-01-24 14:03:26 +0100

    fix(security): changed ResourceFilters et securedAction types

--- src/main/java/fr/openent/competences/security/HasExportLSURight.java
+++ src/main/java/fr/openent/competences/security/HasExportLSURight.java
@@ -11,6 +11,6 @@ import org.entcore.common.user.UserInfos;
 public class HasExportLSURight implements ResourcesProvider {
     @Override
     public void authorize(HttpServerRequest resourceRequest, Binding binding, UserInfos user, Handler<Boolean> handler) {
-        handler.handle(new WorkflowActionUtils().hasRight(user, WorkflowActions.EXPORT_LSU.toString()));
+        handler.handle(WorkflowActionUtils.hasRight(user, WorkflowActions.EXPORT_LSU.toString()));
     }
 }
