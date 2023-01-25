b207eb5c304ecd3d9830f09a176c9224d16dc3f6
BOES <gregoryboes33@gmail.com>
2023-01-25 10:05:08 +0100

    fix(test): EVAL-457 AdminstratorRight test

--- src/test/java/fr/openent/competences/security/AdministratorRightTest.java
+++ src/test/java/fr/openent/competences/security/AdministratorRightTest.java
@@ -1,5 +1,6 @@
 package fr.openent.competences.security;
 
+import fr.openent.competences.constants.Field;
 import fr.openent.competences.security.utils.WorkflowActions;
 import fr.wseduc.webutils.http.Binding;
 import io.netty.handler.codec.http.DefaultHttpHeaders;
@@ -24,10 +25,11 @@ public class AdministratorRightTest {
     HttpServerRequest request;
     Binding binding;
     UserInfos user = new UserInfos();
-
     List<UserInfos.Action> actions;
-
+    MultiMap params;
     UserInfos.Action role1;
+    UserInfos.Action role2;
+    List<String> structures;
 
     @Before
     public void setUp() throws NoSuchFieldException {
@@ -36,15 +38,24 @@ public class AdministratorRightTest {
         binding = Mockito.mock(Binding.class);
         user = new UserInfos();
         role1 = new UserInfos.Action();
+        role2 = new UserInfos.Action();
         actions = new ArrayList<>();
+        structures = new ArrayList<String>();
+        params = Mockito.spy(new HeadersAdaptor(new DefaultHttpHeaders()));
     }
 
 
     @Test
     public void testAuthorize(TestContext ctx){
         role1.setDisplayName(WorkflowActions.ADMIN_RIGHT.toString());
+        role2.setDisplayName(WorkflowActions.COMPETENCES_ACCESS.toString());
         actions.add(role1);
+        actions.add(role2);
         user.setAuthorizedActions(actions);
+        params.set(Field.IDETABLISSEMENT,"111111");
+        Mockito.doReturn(params).when(request).params();
+        structures.add("111111");
+        user.setStructures(structures);
         Async async = ctx.async();
         access.authorize(request, binding, user, result -> {
             ctx.assertEquals(true, result);
@@ -56,8 +67,33 @@ public class AdministratorRightTest {
     @Test
     public void testAuthorizeBadRights(TestContext ctx){
         role1.setDisplayName(WorkflowActions.ACCESS_CONSEIL_DE_CLASSE.toString());
+        role2.setDisplayName(WorkflowActions.COMPETENCES_ACCESS.toString());
         actions.add(role1);
+        actions.add(role2);
         user.setAuthorizedActions(actions);
+        params.set(Field.IDETABLISSEMENT,"111111");
+        Mockito.doReturn(params).when(request).params();
+        structures.add("111111");
+        user.setStructures(structures);
+        Async async = ctx.async();
+        access.authorize(request, binding, user, result -> {
+            ctx.assertEquals(false, result);
+            async.complete();
+        });
+        async.awaitSuccess(10000);
+    }
+
+    @Test
+    public void testAuthorizeBadStructure(TestContext ctx){
+        role1.setDisplayName(WorkflowActions.ADMIN_RIGHT.toString());
+        role2.setDisplayName(WorkflowActions.COMPETENCES_ACCESS.toString());
+        actions.add(role1);
+        actions.add(role2);
+        user.setAuthorizedActions(actions);
+        params.set(Field.IDETABLISSEMENT,"111111");
+        Mockito.doReturn(params).when(request).params();
+        structures.add("11aaaa");
+        user.setStructures(structures);
         Async async = ctx.async();
         access.authorize(request, binding, user, result -> {
             ctx.assertEquals(false, result);
