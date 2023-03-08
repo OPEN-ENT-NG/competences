9ce5f186af9789a89e7a2123b0ec0890037d7020
Q3 <quentin.picard@cgi.com>
2023-03-08 20:38:56 +0100

    wip

---
+++ src/main/resources/public/ts/core/enum/print.enum.ts
@@ -0,0 +1,10 @@
+export enum PRINT_OPTIONS {
+    PRINT_CLASS_REPORT = 'printClasseReleve',
+}
+
+
+export enum CLASS_REPORT_URI_OPTIONS {
+    SCORES = "scores=true",
+    SKILLS = "skills=true",
+    SCORES_AND_SKILLS = "scores=true&skills=true",
+}
