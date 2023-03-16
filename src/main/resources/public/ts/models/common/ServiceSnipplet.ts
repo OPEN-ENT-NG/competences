22642dc1033d6b4a66b2f5f95f6583f66fbb4f77
simon.chaval <69917847+simonchaval@users.noreply.github.com>
2023-03-16 09:45:50 +0100

    fix(security): #EVAL-480 added resource filter, parameter and unit test DevoirController (#200)
    * fix(security): #EVAL-480 add query param idEtablissement to route get /devoirs/done and resourceFilter to check structure on api
    
    * fix(security): #EVAL-480 add resourceFilter on api get /devoirs
    
    * fix(security): #EVAL-480 add resource filter on api @Put(/devoirs/service) and idEtablissement as query param in ServiceSnipplet.ts
    
    * fix(security): #EVAL-480 added resource filter, parameter and unit test DevoirController
    
    * fix(security): #EVAL-480 replace idEtablissement by structureId in front API calls
    
    * fix(security): #EVAL-480 replace AccessVisco by AccessViesco in ModaliteController
    
    * fix(security): #EVAL-480 remove AccessViscoParamServiceStructureTest
    
    ---------
    
    Co-authored-by: BOES <gregoryboes33@gmail.com>

--- src/main/resources/public/ts/models/common/ServiceSnipplet.ts
+++ src/main/resources/public/ts/models/common/ServiceSnipplet.ts
@@ -197,6 +197,7 @@ export class Service {
     async getDevoirsService(){
         try {
             let url = "/competences/devoirs/service";
+            url += `?idEtablissement=` + this.id_etablissement;
             if(this.hasCompetencesParams()) {
                 url += `?id_matiere=${this.id_matiere}` +
                     `&id_enseignant=${this.id_enseignant}` +
@@ -215,7 +216,7 @@ export class Service {
 
     updateDevoirsService(devoirs, matiere) {
         try {
-            return http.put("/competences/devoirs/service", {
+            return http.put("/competences/devoirs/service?structureId=" + this.id_etablissement, {
                 id_devoirs: devoirs,
                 id_matiere: matiere
             });
