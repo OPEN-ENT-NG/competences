d1f287c0f5c2e4ac2d26eed949df4bc66ec1cead
qpic-cgi <55873580+qpic-cgi@users.noreply.github.com>
2023-09-12 20:08:09 +0200

    feat(student-view) #EVAL-569: fix showing scores in year view from periodic statement in student account. (#266)

--- src/main/resources/public/ts/models/eval_parent_mdl.ts
+++ src/main/resources/public/ts/models/eval_parent_mdl.ts
@@ -141,7 +141,7 @@ export class Evaluations extends Model {
                             uri += '&idClasse=' + classeId;
                         }
 
-                        if (idPeriode !== undefined) {
+                        if (idPeriode !== null && idPeriode > -1) {
                             uri += '&idPeriode=' + idPeriode;
                         }
 
@@ -201,7 +201,7 @@ export class Evaluations extends Model {
 
                                 // RECUPERATION DES ANNOTATIONS
                                 let uriAnnotations = Evaluations.api.GET_ANNOTATION + '?idEleve=' + userId;
-                                if (idPeriode !== undefined) {
+                                if (idPeriode !== null && idPeriode > -1) {
                                     uriAnnotations += '&idPeriode=' + idPeriode;
                                 }
 
