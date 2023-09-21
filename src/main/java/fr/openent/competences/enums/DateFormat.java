16bf0c0bd56c384d53781e360a1b1aff2c470b37
qpic-cgi <55873580+qpic-cgi@users.noreply.github.com>
2023-09-21 14:27:38 +0200

    fix(student-tracking) #EVAL-583: fix parsing date in PDF export. (#272)

--- src/main/java/fr/openent/competences/enums/DateFormat.java
+++ src/main/java/fr/openent/competences/enums/DateFormat.java
@@ -1,8 +1,8 @@
 package fr.openent.competences.enums;
 
 public enum DateFormat {
-    SQL_FORMAT("yyyy-MM-dd'T'HH:mm:ss"),
-    MONGO_FORMAT("yyyy-MM-dd HH:mm:ss");
+    SQL_FORMAT("yyyy-MM-dd'T'HH:mm:ss.SSS"),
+    MONGO_FORMAT("yyyy-MM-dd HH:mm:ss.SSS");
     private String format;
     DateFormat(String format) {
         this.format = format;
