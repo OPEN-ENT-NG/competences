8dafeeb322cf5cb5b42229f5d5a011e4a82be8d0
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2024-03-19 11:11:55 +0100

    fix: build.sh publish

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ buildCss() {
 publish () {
   if [ -e "?/.gradle" ] && [ ! -e "?/.gradle/gradle.properties" ]; then
 	  
-    echo "odeUsername=$NEXUS_ODE_USERNAME" >"?/.gradle/gradle.properties"
+    echo "odeUsername=$NEXUS_ODE_USERNAME" > "?/.gradle/gradle.properties"
     echo "odePassword=$NEXUS_ODE_PASSWORD" >>"?/.gradle/gradle.properties"
     echo "sonatypeUsername=$NEXUS_SONATYPE_USERNAME" >>"?/.gradle/gradle.properties"
     echo "sonatypePassword=$NEXUS_SONATYPE_PASSWORD" >>"?/.gradle/gradle.properties"
