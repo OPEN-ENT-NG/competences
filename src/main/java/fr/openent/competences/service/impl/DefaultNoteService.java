8075d5fc3098eee90c434f81daf591a026f9587d
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2025-09-09 15:45:35 +0200

    add logs

--- src/main/java/fr/openent/competences/service/impl/DefaultNoteService.java
+++ src/main/java/fr/openent/competences/service/impl/DefaultNoteService.java
@@ -3431,13 +3431,16 @@ public class DefaultNoteService extends SqlCrudService implements NoteService {
                 JsonObject moyenne = new JsonObject().put(Field.MOYENNE, moyenneComputed)
                         .put(Field.HASNOTE, hasNote);
                 if (!hasNote) {
+                    System.out.println("niko1 : no note for student " + idEleve);
                     // niko : checker ici si 3eme
                     userService.isUserInThirdClassLevel(idEleve)
                         .onSuccess(
                             isInThirdClass -> {
                                 if (isInThirdClass) {
+                                    System.out.println("niko2 : is in 3eme");
                                     moyenne.put(Field.MOYENNE, "EA");
                                 } else {
+                                    System.out.println("niko3 : is NOT in 3eme");
                                     moyenne.put(Field.MOYENNE, "NN");
                                 }
                             }
