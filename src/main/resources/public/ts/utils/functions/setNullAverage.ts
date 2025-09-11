7bec5f461190aef8f092afaa5f73685fd4fbf14b
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2025-09-11 10:45:24 +0200

    add logs

--- src/main/resources/public/ts/utils/functions/setNullAverage.ts
+++ src/main/resources/public/ts/utils/functions/setNullAverage.ts
@@ -2,11 +2,14 @@ import { getEA, getNN } from "./utilsNN";
 
 export const setNullAverage = (eleves) => {
     eleves.forEach(eleve => {
+        console.log("niko : ", eleve);
         if (!eleve.moyenne || eleve.moyenne == getNN()) {
             if (eleve.isUserInThirdClassLevel) {
+                console.log("niko1");
                 eleve.moyenne = getEA();
             }
             else {
+                console.log("niko2");
                 eleve.moyenne = getNN();
             }
         }
