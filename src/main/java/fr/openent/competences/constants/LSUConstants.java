ced01af239c5a911ff1396b8b4bee7b67db3f181
Florent Mariotti <florentcorse@gmail.com>
2026-06-02 14:38:50 +0200

    feat(bfc) : #EVAL-671 update XSD version to 13.0 (#294)
    Co-authored-by: Florent Mariotti <florent.mariotti@cgi.com>

--- src/main/java/fr/openent/competences/constants/LSUConstants.java
+++ src/main/java/fr/openent/competences/constants/LSUConstants.java
@@ -1,5 +1,5 @@
 package fr.openent.competences.constants;
 
 public class LSUConstants {
-    public static final String DEFAULT_SCHEMA_VERSION_VALUE = "10.0";
+    public static final String DEFAULT_SCHEMA_VERSION_VALUE = "13.0";
 }
